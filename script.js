6a960f9a56020ebe4d4acc2484796594703efd1b
gofurrrr <gofur0768@gmail.com>
2026-02-24 20:23:37 +0600

    added full funtionality in all , interview and rejected button and changes and fixed some code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectList = [];
+let currentStatus = 'all'
 
 let total = document.getElementById('total');
 let interviewCount = document.getElementById('interviewCount');
@@ -11,7 +12,7 @@ const rejectedFunctionBtn = document.getElementById('rejected-function-btn')
 
 const allCardsSection = document.getElementById('allCards');
 const mainContainer = document.querySelector('main')
-// const newSection = document.getElementById('new-section')
+const newSection = document.getElementById('new-section')
 
 
 
@@ -34,26 +35,47 @@ function toggleMove(id){
 
 
    const selected = document.getElementById(id)
+   currentStatus = id
 
 
    selected.classList.remove('bg-blue-100', 'text-black')
    selected.classList.add('bg-blue-500', 'text-amber-50')
+
+   if(id == 'interview-function-btn'){
+    allCardsSection.classList.add('hidden');
+    newSection.classList.remove('hidden')
+   }else if(id== 'all-function-btn'){
+    allCardsSection.classList.remove('hidden');
+    newSection.classList.add('hidden')
+   }else if(id=='rejected-function-btn'){
+    allCardsSection.classList.add('hidden');
+    newSection.classList.remove('hidden')
+    renderRejected()
+   }
+
 }
 
 mainContainer.addEventListener('click', function(event){
-    const parenNode = event.target.parentNode.parentNode;
+
+    
+   
+    if(event.target.classList.contains('interview-btn')){
+
+     const parenNode = event.target.parentNode.parentNode;
     const companyName = parenNode.querySelector('.companyName').innerText
     const jobRole = parenNode.querySelector('.jobRole').innerText
     const details = parenNode.querySelector('.details').innerText
     const status  = parenNode.querySelector('.status ').innerText
     const notes  = parenNode.querySelector('.notes ').innerText
-   
+
+   parenNode.querySelector('.status ').innerText ='Interview'
+
    const cardInfo ={
 
        companyName,
        jobRole,
        details,
-       status,
+       status: 'Interview',
        notes
    }
    
@@ -62,10 +84,136 @@ const companyExist = interviewList.find(item => item.companyName == cardInfo.com
    if (!companyExist) {
     interviewList.push(cardInfo)
    }
-   console.log(interviewList);
+
+   rejectList = rejectList.filter(item=> item.companyName != cardInfo.companyName)
+  calculateCount() 
+  renderInterview()
+
+    }
+
+    else if(event.target.classList.contains('rejected-btn')){
+
+     const parenNode = event.target.parentNode.parentNode;
+    const companyName = parenNode.querySelector('.companyName').innerText
+    const jobRole = parenNode.querySelector('.jobRole').innerText
+    const details = parenNode.querySelector('.details').innerText
+    const status  = parenNode.querySelector('.status ').innerText
+    const notes  = parenNode.querySelector('.notes ').innerText
+
+   parenNode.querySelector('.status ').innerText ='Rejected'
+
+   const cardInfo ={
+
+       companyName,
+       jobRole,
+       details,
+       status: 'Rejected',
+       notes
+   }
+   
+const companyExist = rejectList.find(item => item.companyName == cardInfo.companyName)
+
+   if (!companyExist) {
+    rejectList.push(cardInfo)
+   }
+
+    interviewList = interviewList.filter(item=> item.companyName != cardInfo.companyName)
+
+    if(currentStatus == "interview-function-btn"){
+        renderInterview();
+    }
+
+  calculateCount() 
+ 
+//   renderRejected()
+
+    }
+
 
 }
  )
+
+
+ function renderInterview () {
+    newSection.innerHTML = ''
+
+    for(let inter of interviewList){
+        console.log(inter);
+        let div = document.createElement('div');
+        div.className = 'card flex justify-between pt-6 pl-5 pb-8'
+        div.innerHTML = `
+              <div class=" space-y-5">
+                    <!-- part 1 -->
+                     <div>
+                        <p class="companyName  font-semibold text-2xl text-[#002C5C]">${inter.companyName}</p>
+                        <p class="jobRole text-[#323B49]">Frontend Developer</p>
+                        <br>
+                        <p class="details  text-[#323B49]">Dhaka, Bangladesh - Full-time - Salary: 40,000 BDT</p>
+                     </div>
+                     <!-- part 2 -->
+                      <div>
+                        <button class="status   text-[#002C5C] bg-sky-100 px-2 py-2 mb-3 rounded-[5px]">${inter.status}</button>
+                        <p class="notes">Build responsive UI using HTML, CSS, and JavaScript.</p>
+                      </div>
+                    <!-- part 3 -->
+                     <div>
+                        <button class="interview-btn px-5 py-2 text-green-400 border-2  rounded-md ">Interview</button>
+                        <button class="rejected-btn  px-5 py-2 text-red-400 border-2 rounded-md ">Rejected</button>
+                     </div>
+                 </div>
+
+                <!--main part-2-->
+                <div >
+                    
+                    <button class="delete-btn "><img src="./Group 1.png" alt=""></button>
+                </div>
+        `
+        newSection.appendChild(div)
+    }
+
+
+ }
+
+
+ function renderRejected () {
+    newSection.innerHTML = ''
+
+    for(let reject of rejectList){
+       
+        let div = document.createElement('div');
+        div.className = 'card flex justify-between pt-6 pl-5 pb-8'
+        div.innerHTML = `
+              <div class=" space-y-5">
+                    <!-- part 1 -->
+                     <div>
+                        <p class="companyName  font-semibold text-2xl text-[#002C5C]">${reject.companyName}</p>
+                        <p class="jobRole text-[#323B49]">Frontend Developer</p>
+                        <br>
+                        <p class="details  text-[#323B49]">Dhaka, Bangladesh - Full-time - Salary: 40,000 BDT</p>
+                     </div>
+                     <!-- part 2 -->
+                      <div>
+                        <button class="status   text-[#002C5C] bg-sky-100 px-2 py-2 mb-3 rounded-[5px]">${reject.status}</button>
+                        <p class="notes">Build responsive UI using HTML, CSS, and JavaScript.</p>
+                      </div>
+                    <!-- part 3 -->
+                     <div>
+                        <button class="interview-btn px-5 py-2 text-green-400 border-2  rounded-md ">Interview</button>
+                        <button class="rejected-btn  px-5 py-2 text-red-400 border-2 rounded-md ">Rejected</button>
+                     </div>
+                 </div>
+
+                <!--main part-2-->
+                <div >
+                    
+                    <button class="delete-btn "><img src="./Group 1.png" alt=""></button>
+                </div>
+        `
+        newSection.appendChild(div)
+    }
+
+
+ } 
 
 
 
